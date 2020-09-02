dccb76b2191c6eaca5e93c897ce0111ad527a0f3
Debzou <david.petit@etu.univ-smb.fr>
2020-09-02 13:25:41 +0200

    svg size

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,14 @@
+// check the import
 const filename = "main.js";
 console.log(`${filename} is imported`);
 
+// define containe
 const svgContainer = d3.select("#card")
     .append("svg")
     .attr("width",300)
     .attr("height",300);
 
+// add pattern 
 const card = svgContainer.append('svg:image')
     .attr('href','./img/none.png')
     .attr('width', "250")
