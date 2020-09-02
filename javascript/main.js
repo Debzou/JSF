b5c40c3c67257248eae7b118e38e23ad670ed81a
Debzou <david.petit@etu.univ-smb.fr>
2020-09-02 13:24:08 +0200

    add image in svg

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,12 @@
 const filename = "main.js";
 console.log(`${filename} is imported`);
+
+const svgContainer = d3.select("#card")
+    .append("svg")
+    .attr("width",300)
+    .attr("height",300);
+
+const card = svgContainer.append('svg:image')
+    .attr('href','./img/none.png')
+    .attr('width', "250")
+    .attr('height', "250");
