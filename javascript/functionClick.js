2e240babb53f0a4b67d692a04aa20d5f56e417ea
Debzou <david.petit@etu.univ-smb.fr>
2020-09-03 10:37:09 +0200

    add mana

--- javascript/functionClick.js
+++ javascript/functionClick.js
@@ -4,6 +4,25 @@
 const filename2 = "functionClick.js";
 console.log(`${filename2} is imported`);
 
+// event click on button
 $("#generateCard").click(function() {
-    console.log($("input#mana").val())
+    // gather input form
+    let mana = $("input#mana").val();
+    let health = $("input#health").val();
+    let attack = $("input#attack").val();
+    // check length
+    // depending on the size, the coordinates will change.
+    if (mana.toString().length < 2){
+        // postion
+        $('text#manatxt').attr('x',mx1);
+        $('text#manatxt').attr('y',my1);
+        // value
+        $('text#manatxt').text(mana);
+    }else{
+        // postion
+        $('text#manatxt').attr('x',mx2);
+        $('text#manatxt').attr('y',my2);
+        // value
+        $('text#manatxt').text(mana);
+    }
 });
